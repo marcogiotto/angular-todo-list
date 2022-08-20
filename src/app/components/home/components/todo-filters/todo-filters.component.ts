cd9bf289faabb3a811c792b762b29898825d5c48
marco-gioto <marcogioto.mg@gmail.com>
2022-08-20 17:37:40 -0300

    general styles

--- src/app/components/home/components/todo-filters/todo-filters.component.ts
+++ src/app/components/home/components/todo-filters/todo-filters.component.ts
@@ -1,23 +1,49 @@
 import { FormControl } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { taskStatus } from './../../models/task-status';
+import { Subject, take, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-todo-filters',
   templateUrl: './todo-filters.component.html',
   styleUrls: ['./todo-filters.component.scss']
 })
-export class TodoFiltersComponent implements OnInit {
+export class TodoFiltersComponent implements OnInit, OnDestroy {
   taskName: FormControl<string | null>;
   taskStatus: FormControl<string | null>;
-
+  private unsubscribe$ = new Subject();
   constructor() {
     this.taskName = new FormControl('');
     this.taskStatus = new FormControl(taskStatus.all);
   }
 
   ngOnInit(): void {
+    this.listenTotaskNameChange();
+    this.listenToTaskStatusChange();
+  }
+
+  private listenTotaskNameChange() {
+
+    this.taskName.valueChanges.pipe(
+      takeUntil(this.unsubscribe$)
+    ).subscribe(val => {
+      console.log(val);
+    })
+  }
+
+  private listenToTaskStatusChange() {
+
+    this.taskStatus.valueChanges.pipe(
+      takeUntil(this.unsubscribe$)
+    ).subscribe(val => {
+      console.log(val);
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next('');
+    this.unsubscribe$.complete();
   }
 
 }
