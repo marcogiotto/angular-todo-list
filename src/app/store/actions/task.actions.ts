7fcd52a183233c150b94dee5597fc32d6513eb0d
marco-gioto <marcogioto.mg@gmail.com>
2022-08-21 20:48:12 -0300

    add changeStatus action

--- src/app/store/actions/task.actions.ts
+++ src/app/store/actions/task.actions.ts
@@ -1,3 +1,4 @@
+import { taskStatus } from './../../components/home/models/task-status';
 import { createAction, props } from "@ngrx/store";
 
 export const addTask = createAction(
@@ -8,4 +9,9 @@ export const addTask = createAction(
 export const deleteTask = createAction(
     '[Todo] delete task',
     props<{ taskId: number }>()
+);
+
+export const changeStatusTask = createAction(
+    '[Todo] change status task',
+    props<{ taskId: number, taskStatus: taskStatus }>()
 )
--- src/app/store/reducers/task.reducer.ts
+++ src/app/store/reducers/task.reducer.ts
@@ -7,5 +7,16 @@ export const initialState: TaskModel[] = [];
 export const taskReducer = createReducer(
     initialState,
     on(taskActions.addTask, (state, { taskName }) => [...state, new TaskModel(taskName)]),
-    on(taskActions.deleteTask, (state, { taskId }) => state.filter(item => item.id !== taskId))
+    on(taskActions.deleteTask, (state, { taskId }) => state.filter(item => item.id !== taskId)),
+    on(taskActions.changeStatusTask, (state, { taskId, taskStatus }) => {
+        const newState = [...state];
+
+        return newState.map(task => {
+            if (task.id === taskId) {
+                task.status = taskStatus;
+            }
+            return task
+
+        })
+    })
 )
